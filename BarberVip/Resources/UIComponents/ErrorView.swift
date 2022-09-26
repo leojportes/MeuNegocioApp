007d1c02a55021db93a88e8d42cb37da362c79ff
Renilson Moreira <renilsonmoreira_@hotmail.com>
2022-09-26 17:18:08 -0300

    cria componente ErrorView e ErrorTableViewCell

---
+++ BarberVip/Resources/UIComponents/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorView.swift
+//  BarberVip
+//
+//  Created by Renilson Moreira on 26/09/22.
+//
+
+import UIKit
+
+class ErrorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
