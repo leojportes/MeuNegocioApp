215c7f6575a1406ee9da6990d868b57db9d2325c
Renilson Moreira <renilsonmoreira_@hotmail.com>
2022-09-29 14:15:24 -0300

    ajusta altura da celula

--- BarberVip/Sources/Home/View/HomeView.swift
+++ BarberVip/Sources/Home/View/HomeView.swift
@@ -242,7 +242,7 @@ extension HomeView: UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
+        return 80
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
