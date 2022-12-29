3671da6bb272de43c9bfac5e52f9e7ea40e6d81e
Renilson Moreira <renilsonmoreira_@hotmail.com>
2022-12-29 20:02:29 -0300

    adiciona replacing a campo de custos

--- MeuNegocio/Sources/AddProcedure/CardCostsView.swift
+++ MeuNegocio/Sources/AddProcedure/CardCostsView.swift
@@ -89,9 +89,16 @@ class CardCostsView: UIView {
     
     @objc
     private func myTextFieldDidChange(_ textField: UITextField) {
-        guard let value = textField.text else { return }
-
-        valueTextField(value)
+        if let amountString = textField.text?.currencyInputFormatting() {
+            textField.text = amountString
+            let amount = amountString
+                .replacingOccurrences(of: "R$", with: "")
+                .dropFirst()
+                .replacingOccurrences(of: ".", with: "")
+                .replacingOccurrences(of: ",", with: ".")
+                .replacingOccurrences(of: " ", with: ".")
+            valueTextField(amount)
+        }
     }
 }
 extension CardCostsView: ViewCodeContract {
